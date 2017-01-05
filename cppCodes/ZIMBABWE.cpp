e473feb01423fa50947802ca000b125f98faa690
byungkim <byungkim@ebay.com>
2017-01-05 21:39:40 +0900

    으아 오늘은 여기까지...

--- cppCodes/ZIMBABWE.cpp
+++ cppCodes/ZIMBABWE.cpp
@@ -3,18 +3,34 @@
 #include <utility>
 #include <algorithm>
 #include <cstdio>
+#include <string>
 
 using namespace std;
 
+string e, digits;
+int n, m;
+int cache[1<<14][20][2];
+
 void Reset()
 {
-	
+	memset(cache, 0, sizeof(cache));
+}
+
+int price(int index, int taken, int mod, int less)
+{
+	if (index == n)
+		return (mod == 0 && less == 1) ? 1 : 0;
+	int& ret = cache[taken][mod][less];
 }
 
 void Solve()
 {
 	Reset();
-	
+	cin>>e>>m;
+	n = e.length();
+	digits = e;
+	sort(digits.begin(), digits.end());
+	cout<<price(0, 0, 0, 1)<<endl;
 }
 
 int main(int argc, const char * argv[]) {
