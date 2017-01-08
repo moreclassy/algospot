b0f57019f02d1c5cc10870f54aad054248da241d
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-01-08 12:54:11 +0900

    Please enter the commit message for your changes. Lines starting

--- cppCodes/ZIMBABWE.cpp
+++ cppCodes/ZIMBABWE.cpp
@@ -5,60 +5,65 @@
 #include <cstring>
 #include <cmath>
 
-#define PI 3.14159265
+#define _USE_MATH_DEFINES
 
 using namespace std;
 
-double p, x, y;
+const int MOD = 1000000007;
+string e, digits;
+int n, m;
+int cache[1<<14][20][2];
 
-double Theta(double x_, double y_)
+void Reset()
 {
-    double arctan = atan2(x_, y_) * 180.0 / PI;
-    
-    if (x_ >= 0 && y_ >= 0)
-        return arctan;
-    if (x_ >= 0 && y_ <= 0)
-        return 180 - arctan;
-    if (x_ <= 0 && y_ <= 0)
-        return 180 + arctan;
-    if (x_ <= 0 && y_ <= 0)
-        return 360 - arctan;
-    
-    return arctan;
+    memset(cache, -1, sizeof(cache));
 }
 
-bool IsWhite()
+int Price(int index, int taken, int mod, int less)
 {
-    double x_ = x - 50.0, y_ = y - 50.0;
+    if (index == n)
+        return (less && mod == 0) ? 1 : 0;
     
-    // out of circle or center
-    if (x_*x_ + y_*y_ > 50.0*50.0 || (x_ == 0 && y_ == 0))
-        return true;
-    
-    if (Theta(x_, y_) <= p*3.6)
-        return false;
-    
-    return true;
+    int& ret = cache[taken][mod][less];
+    if (ret != -1) return ret;
+    ret = 0;
+    for (int next = 0; next < n; ++next) if ((taken & (1<<next)) == 0)
+    {
+        if(!less && e[index] < digits[next])
+            continue;
+        if (next > 0 && digits[next -1] == digits[next] && (taken & (1<<(next -1))) == 0)
+            continue;
+        int nextTaken = taken | (1<<next);
+        int nextMod = (mod*10 + digits[next] - '0') % m;
+        int nextLess = less || e[index] > digits[next];
+        ret += Price(index +1, nextTaken, nextMod, nextLess);
+        ret %= MOD;
+    }
+    return ret;
 }
 
-void Solve(int caseNum)
+void Solve()
 {
-    cin>>p>>x>>y;
+    Reset();
+    cin>>e>>m;
+    digits = e;
+    n = (int)e.length();
+    sort(digits.begin(), digits.end());
     
-    cout<<"Case #"<<caseNum<<": "<<(IsWhite()?"white":"black")<<endl;
+    cout<<Price(0, 0, 0, 0)<<endl;
 }
 
 int main(int argc, const char * argv[]) {
-    //freopen("input.txt","r",stdin);	freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin);	freopen("output.txt","w",stdout);
     
     int caseCnt = 0;
     cin>>caseCnt;
     
     for (int i = 0; i < caseCnt; i++) {
-        Solve(i+1);
+        Solve();
     }
     
-    //fclose(stdin);	fclose(stdout);
+    fclose(stdin);	fclose(stdout);
     
     return 0;
 }
