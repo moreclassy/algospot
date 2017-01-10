05331bebfdca06fa66a54a78f03168dce04fb8b3
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-01-10 12:27:50 +0900

    genius의 squarematrix 의 기본 구조 완성

--- cppCodes/GENIUS.cpp
+++ cppCodes/GENIUS.cpp
@@ -13,6 +13,67 @@ using namespace std;
 int n, k, m, length[50], favorite[10];
 double T[50][50];
 
+class SquareMatrix
+{
+    public :
+    vector<vector<double>> matrix;
+    
+    SquareMatrix() { }
+    SquareMatrix(int size)
+    {
+        matrix = *new vector<vector<double>>(size);
+        for (int i = 0; i < size; i++)
+            matrix[i] = *new vector<double>(size, 0);
+    }
+    
+    SquareMatrix& operator=(SquareMatrix& other)
+    {
+        matrix = other.matrix;
+        
+        return *this;
+    }
+    
+    int size()
+    {
+        return (int)matrix.size();
+    }
+    
+    vector<double>& operator[](int idx)
+    {
+        return matrix[idx];
+    }
+    
+    SquareMatrix& operator*(SquareMatrix& other)
+    {
+        if ((int)matrix.size() != other.size())
+        {
+            throw "행렬 크기 안맞음";
+        }
+        
+        SquareMatrix tmpMatrix = *this;
+        
+        for (int i = 0; i < other.size(); i++) {
+            for (int j = 0; j < other.size(); j++) {
+                double tmp = 0;
+                for (int k = 0; k < other.size(); k++)
+                    tmp += tmpMatrix[i][k] * other[k][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        
+        return *this;
+    }
+    
+    void print()
+    {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+};
+
 void Reset()
 {
     memset(T, 0, sizeof(T));
@@ -41,13 +102,24 @@ void Solve()
 int main(int argc, const char * argv[]) {
     freopen("input.txt","r",stdin);	freopen("output.txt","w",stdout);
     
-    int caseCnt = 0;
+    SquareMatrix W(3);
+    W[0][0] = 1; W[0][1] = 2; W[0][2] = 0;
+    W[1][0] = 2; W[1][1] = 0; W[1][2] = 3;
+    W[2][0] = 2; W[2][1] = 0; W[2][2] = 1;
+    
+    SquareMatrix Z = W;
+    Z[0][0] = 5;
+    
+    Z = (Z * W);
+    Z.print();
+    
+    /*int caseCnt = 0;
     cin>>caseCnt;
     
     for (int i = 0; i < caseCnt; i++) {
         Solve();
-    }
-    
+    }*/
+     
     fclose(stdin);	fclose(stdout);
     
     return 0;
