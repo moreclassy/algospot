fcd5a15b14e6292a82ba6dff5e4e1fb721fb28c1
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-02-14 22:37:35 +0900

    numbergame

---
+++ cppCodes/NUMBERGAME.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+void Reset()
+{
+    
+}
+
+void GetInput()
+{
+    
+}
+
+void Solve()
+{
+    Reset();
+    GetInput();
+    
+}
+
+int main(int argc, const char * argv[]) {
+    freopen("input.txt","r",stdin);	freopen("output.txt","w",stdout);
+    
+    int caseCnt = 0;
+    cin>>caseCnt;
+    
+    for (int i = 0; i < caseCnt; i++) {
+        Solve();
+    }
+    
+    return 0;
+}
