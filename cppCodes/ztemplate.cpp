ba01ea9b45adf5a9a08bbed570f6332de99a18c2
moreclassy <root@kims-MacBook-Air.local>
2017-03-04 10:37:13 +0900

    탬플릿 이름 변경

---
+++ cppCodes/ztemplate.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <queue>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+void Reset()
+{
+    
+}
+
+void GetInput()
+{
+    
+}
+
+void Solve()
+{
+    Reset();
+    GetInput();
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int caseCnt = 0;
+    cin>>caseCnt;
+    
+    for (int i = 0; i < caseCnt; i++) {
+        Solve();
+    }
+    
+    return 0;
+}
