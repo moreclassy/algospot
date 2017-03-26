f32ac7c17e4523e451d4d725adb187a9030f111d
moreclassy <root@kims-MacBook-Air.local>
2017-03-26 12:09:33 +0900

    ROUTING 시작:

---
+++ cppCodes/ROUTING.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <queue>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int n; // 컴퓨터 수
+int m; // 회선의 수
+int const MAX_N = 10000; // 최대 컴퓨터 수
+vector<pair<int, double>> adj[MAX_N]; // 간선 (정점 번호, 간선 가중치)
+
+void Reset()
+{
+    
+}
+
+void GetInput()
+{
+    
+}
+
+void Solve()
+{
+    Reset();
+    GetInput();
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int caseCnt = 0;
+    cin>>caseCnt;
+    
+    for (int i = 0; i < caseCnt; i++) {
+        Solve();
+    }
+    
+    return 0;
+}
