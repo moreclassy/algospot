56600ce68580539d0155079b0e44e931bbf3b814
byungkim <byungkim@ebay.com>
2017-09-12 13:15:12 +0900

    SNAIL

--- rep2/SNAIL.cpp
+++ rep2/SNAIL.cpp
@@ -4,29 +4,40 @@
 using namespace std;
 
 const double rainy = 0.75;
-const double sunny = 0.35;
+const double sunny = 0.25;
 
 double prob(vector<vector<double>>& dp, int i, int j) {
-    if (i < 0 || j < 0) return 0;
+    if (i < 0 || j < 0 || i < j) return 0;
     double& ret = dp[i][j];
-    if (ret > 0) return ret;
-    ret = prob(dp, i-1, j-1) * rainy + prob(dp, i-1, j-2) * sunny;
+    if (ret >= -0.5) return ret;
+    ret = prob(dp, i-2, j-1) * rainy + prob(dp, i-1, j-1) * sunny;
     return ret;
 }
 
 void solve() {
     int n, m;
     cin>>n>>m;
-    
-    vector<vector<double>> dp(m, vector<double>(n, -1.0));
+	if (m >= n) {
+		cout<<1.0<<endl;
+		return;
+	}
+
+    vector<vector<double>> dp(n, vector<double>(m + 1, -1.0));
     dp[0][0] = 1.0;
     
-    
+    double ret = 1.0;
+	for (int i = m; i < n; i++) {
+		ret -= prob(dp, i, m);
+	}
+	cout<<ret<<endl;
 }
 
 int main() {
     int c;
     cin>>c;
+
+	cout << fixed;         //소수점 고정
+	cout.precision(10);
     
     for (int i = 0; i < c; i++) {
         solve();
