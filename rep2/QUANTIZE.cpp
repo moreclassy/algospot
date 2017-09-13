4be2d41320ed2c3c7f4887242756273d07df997d
byungkim <byungkim@ebay.com>
2017-09-13 17:25:35 +0900

    QUANTIZE

---
+++ rep2/QUANTIZE.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int minError(int from, int to) {
+	
+}
+
+void solve() {
+    int len, parts;
+	cin>>len>>parts;
+
+}
+
+int main()
+{
+    int c;
+    cin>>c;
+    
+    for (int i = 0; i < c; i++) {
+        solve();
+    }
+	
+	return 0;
+}
