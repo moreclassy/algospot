78bb4e762631b201bea5dc5949ea05bce8c8f988
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-09-23 16:33:31 +0900

    ocr

---
+++ rep2/OCR.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+	
+}
+
+int main() {
+	int c;
+	cin>>c;
+
+	for (int i = 0; i < c; i++) {
+		solve();
+	}
+	return 0;
+}
