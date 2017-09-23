177b5d49b9b1a12b5414338eabe728b7d2cec7a1
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-09-23 19:07:48 +0900

    ocr

--- rep2/OCR.cpp
+++ rep2/OCR.cpp
@@ -5,47 +5,69 @@
 
 using namespace std;
 int wCnt, pCnt;
-double first[500], beside[500][500], wrong[500][500];
-string words[500], input[100];
+vector<double> first;
+vector<vector<double>> beside, wrong;
+vector<string> words;
+int input[100];
 map<string, int> strIdx;
 int inCnt;
 int pre[100][500];
+
+void printPre() {
+    for (int i = 0; i < inCnt; i++) {
+        for (int j = 0; j < wCnt; j++)
+            cout<<pre[i][j]<<" ";
+        cout<<endl;
+    }
+}
 
 double getP(vector<vector<double>> &dp, int idx, int w) {
     double &ret = dp[idx][w];
     if (ret > -1.0) return ret;
     
-    ret = 0.0;
-    int inputIdx = strIdx[input[idx]];
     for (int i = 0; i < wCnt; i++) {
-        double pi = getP(dp, idx - 1, i) * beside[i][w] * wrong[w][inputIdx];
+        double pi = getP(dp, idx - 1, i) * beside[i][w] * wrong[w][input[idx]];
         if (ret < pi) {
             pre[idx][w] = i;
+            ret = pi;
         }
     }
+    
     return ret;
 }
 
 void printWord(int idx, int w) {
     if (idx < 0) return;
-    cout<<words[w]<<" ";
     printWord(idx - 1, pre[idx][w]);
+    cout<<words[w]<<" ";
+}
+
+void printTable(vector<vector<double>> &dp) {
+    for (int i = 0; i < dp.size(); i++) {
+        for (int j = 0; j < dp[i].size(); j++)
+            cout<<dp[i][j]<<" ";
+        cout<<endl;
+    }
 }
 
 void solve() {
     cin>>inCnt;
-    for (int i = 0; i < inCnt; i++)
-        cin>>input[i];
+    for (int i = 0; i < inCnt; i++) {
+        string inStr;
+        cin>>inStr;
+        input[i] = strIdx[inStr];
+    }
     
     vector<vector<double>> dp(inCnt, vector<double>(wCnt, -2.0));
-    int firstIdx = strIdx[input[0]];
-    for (int i = 0; i < wCnt; i++)
-        dp[0][i] = first[i] * wrong[i][firstIdx];
     
-    double maxP = 0.0;
+    for (int i = 0; i < wCnt; i++)
+        dp[0][i] = first[i] * wrong[i][input[0]];
+    
+    double maxP = -2.0;
     int lastW = 0;
     for (int i = 0; i < wCnt; i++) {
-        int curP = getP(dp, inCnt - 1, i);
+        double curP = getP(dp, inCnt - 1, i);
+        
         if (curP > maxP) {
             maxP = curP;
             lastW = i;
@@ -58,15 +80,23 @@ void solve() {
 
 int main() {
     cin>>wCnt>>pCnt;
+    
+    words = vector<string>(wCnt);
     for (int i = 0; i < wCnt; i++) {
         cin>>words[i];
         strIdx[words[i]] = i;
     }
+    
+    first = vector<double>(wCnt);
     for (int i = 0; i < wCnt; i++)
         cin>>first[i];
+    
+    beside = vector<vector<double>>(wCnt, vector<double>(wCnt));
     for (int i = 0; i < wCnt; i++)
         for (int j = 0; j < wCnt; j++)
             cin>>beside[i][j];
+    
+    wrong = vector<vector<double>>(wCnt, vector<double>(wCnt));
     for (int i = 0; i < wCnt; i++)
         for (int j = 0; j < wCnt; j++)
             cin>>wrong[i][j];
