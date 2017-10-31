6c71fbbfedfa2947830f9ecc1667c662ce9e5ff2
moreclassy <root@kims-MacBook-Air.local>
2017-11-01 00:55:13 +0900

    graduation

--- rep2/GRADUATION.cpp
+++ rep2/GRADUATION.cpp
@@ -1,45 +1,93 @@
 #include <iostream>
-#include <string>
+#include <bitset>
 #include <vector>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-int subjects = 0, need = 0, termcnt = 0, step = 0;
-int pre[12];
-int term[10];
+const int MAXN = 12;
+const int MAXM = 10;
+const int INF = 987654321;
 
-void solve() {
-    cin>>subjects>>need>>termcnt>>step;
-    
-    for (int i = 0; i < subjects; i++) {
-        pre[i] = 0;
-        int precnt;
-        cin>>precnt;
-        for (int j = 0; j < precnt; j++) {
-            int tpre;
-            cin>>tpre;
-            pre[i] |= (1 << tpre);
-        }
-    }
-    
-    for (int i = 0; < termcnt; i++) {
-        int scnt;
-        cin>>scnt;
-        term[i] = 0;
-        for (int j = 0; j < scnt; j++) {
-            
-        }
-    }
+int n, k, m, l; // 전공 과목 수, 들어야 할 과목 수, 학기 수, 학기당 최대로 들을 수 있는 과목 수
+int prerequisite[MAXN]; // 선수과목
+int classes[MAXM];  // 학기 당 개설 과목
+int cache[MAXM][1<<MAXN];   // dp table [학기][들은과목]
+
+int bitCount(int num) {
+    return (int)bitset<MAXN>(num).count();
 }
 
-int main()
-{
+void getInput() {
+    cin>>n>>k>>m>>l;
+    
+    for (int i = 0; i < n; i++) {
+        int r;  // 선수 과목 수
+        cin>>r;
+        int pre = 0;
+        for (int j = 0; j < r; j++) {
+            int tmp;
+            cin>>tmp;
+            pre |= (1<<tmp);
+        }
+        prerequisite[i] = pre;
+    }
+    
+    for (int i = 0; i < m; i++) {
+        int c;
+        cin>>c;
+        int cl = 0;
+        for (int j = 0; j < c; j++) {
+            int tmp;
+            cin>>tmp;
+            cl |= (1<<tmp);
+        }
+        classes[i] = cl;
+    }
+    
+    memset(cache, -1, sizeof(cache));
+}
+
+int graduate(int semester, int taken) {
+    if (bitCount(taken) >= k) return 0;
+    if (semester == m) return INF;
+    
+    int &ret = cache[semester][taken];
+    if (ret != -1) return ret;
+    ret = INF;
+    
+    int canTake = (classes[semester] & ~taken);
+    
+    for (int i = 0; i < n; i++)
+        if ((canTake & (1<<i)) && ((taken & prerequisite[i]) != prerequisite[i]))
+            canTake &= ~(1<<i);
+    
+    for (int take = canTake; take > 0; take = ((take - 1) & canTake)) {
+        if (bitCount(take) > l) continue;
+        ret = min(ret, graduate(semester + 1, taken | take) + 1);
+    }
+    
+    ret = min(ret, graduate(semester + 1, taken));
+    
+    return ret;
+}
+
+void solve() {
+    getInput();
+    
+    int ret = graduate(0, 0);
+    if (ret < INF)
+        cout<<ret<<endl;
+    else
+        cout<<"IMPOSSIBLE"<<endl;
+}
+
+int main() {
     int c;
     cin>>c;
-    
-    for (int i = 0; i < c; i++) {
+    for (int i = 0; i < c; i++)
         solve();
-    }
-	
-	return 0;
+    
+    return 0;
 }
---
+++ rep2/MINASTIRITH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    int c;
+    cin>>c;
+    for (int i = 0; i < c; i++)
+        solve();
+    
+    return 0;
+}
